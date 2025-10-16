b3765c660309fcb17a2f1098034e0e441befc8f3
jaspritnaveen <jaspritnaveen@gmail.com>
2025-10-16 21:36:44 +0530

    skills page updated

--- src/Components/Pages/Welcome/Welcome.jsx
+++ src/Components/Pages/Welcome/Welcome.jsx
@@ -6,7 +6,13 @@ const Welcome = () => {
       <div className="welcome-mes">
         <section className="welcome-section">
           <div className="welcome-card">
-         
+            <h1>Welcome to My Portfolio</h1>
+            <p>
+              Thank you for visiting my portfolio website! I am excited to share
+              my journey, skills, projects, and experiences with you. Feel free
+              to explore and connect with me if you'd like to collaborate or
+              learn more.
+            </p>
           </div>
         </section>
       </div>
